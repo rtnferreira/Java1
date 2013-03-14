9753c9ae58c10c990629c60a4ef04fb377227b92
Ruben Ferreira <Ruben Ferreira@RubenFerreira>
2013-03-14 16:54:37 +0000

    Update do programa

--- PL5/src/pl5/PL5.java
+++ PL5/src/pl5/PL5.java
@@ -14,6 +14,9 @@ public class PL5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+  
+        System.out.println("linha1");
+        System.out.println("linha2");
     }
 }
